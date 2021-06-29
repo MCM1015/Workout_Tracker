d086cb55b218c1dce3bd226f6019ac52f8e4995c
Mary McClernan <mary.c.mcclernan@gmail.com>
2021-06-29 14:51:46 -0400

    all necessary files created, folders re-organized, server and model done

---
+++ models/Workout.js
@@ -0,0 +1,40 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const workoutSchema = new Schema({
+  day: {
+    type: Date,
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: 'Enter excercise type'
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: 'Enter exercise name'
+      },
+      duration: {
+        type: Number,
+        required: 'Enter exercise duration (minutes)'
+      },
+      weight: {
+        type: Number
+      },
+      reps: {
+        type: Number
+      },
+      sets: {
+        type: Number
+      }
+    }
+  ]
+});
+
+const Workout = mongoose.model("Workout", workoutSchema);
+
+module.exports = Workout;
---
+++ routes/apiRoutes.js
@@ -0,0 +1,5 @@
+//insert get, post, update and delete routes here
+
+//exericse route(s)
+
+//stat route(s)
